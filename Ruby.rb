5323fc37ae96b3f7784025e12cb74e580329ac3c
Narendrakahar <156931033+Narendrakahar@users.noreply.github.com>
2024-07-05 09:23:28 +0530

    Create Ruby.rb

---
+++ Ruby.rb
@@ -0,0 +1,49 @@
+#a=30
+#b=20
+#c=15
+#puts "Value of A is:#{a} & B is:#{20} & C is:#{c}"
+#if a>b && a>c
+ #   puts "A is greater"
+#elsif  b>c
+ #   puts "B is greater"
+#else
+ #   puts "C is greater"
+#end
+
+
+# nested if - 2 program
+# switch 2 program
+# loop - 3 program
+
+# Nested if P1
+
+# package main
+# import ("fmt")
+
+# func main() {
+#   num = 20
+#   if num >= 10 
+#     fmt.Println("Num is more than 10.")
+#     if num > 15 {
+#       fmt.Println("Num is also more than 15.")
+#      }
+#   } else {
+#     fmt.Println("Num is less than 10.")
+  
+# }
+
+# if statement
+a = 30
+if a >=20
+  puts "A is greater than 20."
+end
+
+# if-else statement
+a = 30
+if a <=20
+  puts "A is greater than 20."
+else a >=25
+  puts "A is greater than 25."
+end
+
+# if – elsif ladder
